a18040863a314efa70d9a1dd72940908fcad46bd
Abbos Qambarov <abbosqambarov47@gmail.com>
2024-07-09 14:01:52 +0500

    done payment testing4

--- storage/postgres/payment_test.go
+++ storage/postgres/payment_test.go
@@ -1,1 +1,72 @@
 package postgres
+
+import (
+	"context"
+	pb "payment_service/genproto/payment"
+	"reflect"
+	"testing"
+)
+
+func TestMakePayment(t *testing.T) {
+	db, _ := ConnectDB()
+    defer db.Close()
+
+    repo := NewPaymentRepo(db)
+
+    pay := &pb.PaymentDetails{
+        ReservationId: "550e8400-e29b-41d4-a716-446655444001",
+        Amount:        100.00,
+        PaymentMethod: "cash",
+    }
+
+    _, err := repo.MakePayment(context.Background(), pay)
+    if err != nil {
+        t.Error("Error making payment")
+    }
+}
+
+func TestGetPayment(t *testing.T) {
+	db, _ := ConnectDB()
+    defer db.Close()
+
+    repo := NewPaymentRepo(db)
+
+    id := "550e8400-e29b-41d4-a716-446655444001"
+
+    pay, err := repo.GetPayment(context.Background(), id)
+    if err != nil {
+        t.Error("Error getting payment")
+    }
+
+	exp := pb.PaymentInfo{
+		ReservationId: "550e8400-e29b-41d4-a716-446655444001",
+        Amount:        100.00,
+        PaymentMethod: "cash",
+		PaymentStatus: "pending",
+	}
+    if !reflect.DeepEqual(&exp, pay) {
+        t.Error("Payment ID mismatch")
+    }
+}
+
+func TestUpdatePayment(t *testing.T) {
+	db, _ := ConnectDB()
+    defer db.Close()
+
+    repo := NewPaymentRepo(db)
+
+    id := "550e8400-e29b-41d4-a716-446655440001"
+
+    pay := &pb.PaymentInfo{
+        Id:            id,
+        ReservationId: "550e8400-e29b-41d4-a716-446655440001",
+        Amount:        100.00,
+        PaymentMethod: "cash",
+        PaymentStatus: "completed",
+    }
+
+    err := repo.UpdatePayment(context.Background(), pay)
+    if err!= nil {
+        t.Error("Error updating payment")
+    }
+}
