4083d6d898300e25488e1697287cf7c74af07238
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-07-09 14:48:10 +0500

    testing added

--- storage/postgres/payment_test.go
+++ storage/postgres/payment_test.go
@@ -9,64 +9,65 @@ import (
 
 func TestMakePayment(t *testing.T) {
 	db, _ := ConnectDB()
-    defer db.Close()
+	defer db.Close()
 
-    repo := NewPaymentRepo(db)
+	repo := NewPaymentRepo(db)
 
-    pay := &pb.PaymentDetails{
-        ReservationId: "550e8400-e29b-41d4-a716-446655444001",
-        Amount:        100.00,
-        PaymentMethod: "cash",
-    }
+	pay := &pb.PaymentDetails{
+		ReservationId: "550e8400-e29b-41d4-a716-446655444001",
+		Amount:        100.00,
+		PaymentMethod: "cash",
+	}
 
-    _, err := repo.MakePayment(context.Background(), pay)
-    if err != nil {
-        t.Error("Error making payment")
-    }
+	_, err := repo.MakePayment(context.Background(), pay)
+	if err != nil {
+		t.Error("Error making payment")
+	}
 }
 
 func TestGetPayment(t *testing.T) {
 	db, _ := ConnectDB()
-    defer db.Close()
+	defer db.Close()
 
-    repo := NewPaymentRepo(db)
+	repo := NewPaymentRepo(db)
 
-    id := "550e8400-e29b-41d4-a716-446655444001"
+	id := "a1234567-89ab-cdef-0123-456789abcdef"
 
-    pay, err := repo.GetPayment(context.Background(), id)
-    if err != nil {
-        t.Error("Error getting payment")
-    }
+	pay, err := repo.GetPayment(context.Background(), id)
+	if err != nil {
+		t.Error("Error getting payment")
+	}
 
 	exp := pb.PaymentInfo{
-		ReservationId: "550e8400-e29b-41d4-a716-446655444001",
-        Amount:        100.00,
-        PaymentMethod: "cash",
-		PaymentStatus: "pending",
+		Id:            "a1234567-89ab-cdef-0123-456789abcdef",
+		ReservationId: "550e8400-e29b-41d4-a716-446655440001",
+		Amount:        100.00,
+		PaymentMethod: "cash",
+		PaymentStatus: "completed",
 	}
-    if !reflect.DeepEqual(&exp, pay) {
-        t.Error("Payment ID mismatch")
-    }
+	if !reflect.DeepEqual(&exp, pay) {
+		t.Error("Payment ID mismatch")
+	}
 }
 
 func TestUpdatePayment(t *testing.T) {
 	db, _ := ConnectDB()
-    defer db.Close()
+	defer db.Close()
 
-    repo := NewPaymentRepo(db)
+	repo := NewPaymentRepo(db)
 
-    id := "550e8400-e29b-41d4-a716-446655440001"
+	id := "550e8400-e29b-41d4-a716-446655440001"
 
-    pay := &pb.PaymentInfo{
-        Id:            id,
-        ReservationId: "550e8400-e29b-41d4-a716-446655440001",
-        Amount:        100.00,
-        PaymentMethod: "cash",
-        PaymentStatus: "completed",
-    }
+	pay := &pb.PaymentInfo{
+		Id:            id,
+		ReservationId: "550e8400-e29b-41d4-a716-446655440001",
+		Amount:        100.00,
+		PaymentMethod: "cash",
+		PaymentStatus: "completed",
+	}
 
-    err := repo.UpdatePayment(context.Background(), pay)
-    if err!= nil {
-        t.Error("Error updating payment")
-    }
+	err := repo.UpdatePayment(context.Background(), pay)
+	if err != nil {
+		t.Error("Error updating payment")
+	}
 }
