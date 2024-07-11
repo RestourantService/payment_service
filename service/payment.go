0e554c618f5d0fa1b787836fa33c3e9c95e9f8f2
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-07-11 20:12:50 +0500

    added logging to payment

--- service/payment.go
+++ service/payment.go
@@ -3,8 +3,10 @@ package service
 import (
 	"context"
 	"database/sql"
+	"log/slog"
 	pb "payment_service/genproto/payment"
 	pbr "payment_service/genproto/reservation"
+	"payment_service/pkg/logger"
 	"payment_service/storage/postgres"
 
 	"github.com/pkg/errors"
@@ -14,55 +16,83 @@ type PaymentService struct {
 	pb.UnimplementedPaymentServer
 	Repo              *postgres.PaymentRepo
 	ReservationClient pbr.ReservationClient
+	Logger            *slog.Logger
 }
 
 func NewPaymentService(db *sql.DB, reservation pbr.ReservationClient) *PaymentService {
 	return &PaymentService{
 		Repo:              postgres.NewPaymentRepo(db),
 		ReservationClient: reservation,
+		Logger:            logger.NewLogger(),
 	}
 }
 
 func (p *PaymentService) CreatePayment(ctx context.Context, req *pb.PaymentDetails) (*pb.Status, error) {
+	p.Logger.Info("CreatePayment method is starting")
+
 	status, err := p.ReservationClient.ValidateReservation(ctx, &pbr.ID{Id: req.ReservationId})
 	if err != nil {
-        return nil, errors.Wrap(err, "failed to validate reservation")
-    }
-    if !status.Successful {
-        return nil, errors.New("reservation validation failed")
-    }
+		err := errors.Wrap(err, "failed to validate reservation")
+		p.Logger.Error(err.Error())
+		return nil, err
+	}
+	if !status.Successful {
+		err := errors.New("reservation validation failed")
+		p.Logger.Error(err.Error())
+		return nil, err
+	}
+
+	p.Logger.Info("Reservation has been validated")
 
 	resp, err := p.Repo.CreatePayment(ctx, req)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to make payment")
+		err := errors.Wrap(err, "failed to make payment")
+		p.Logger.Error(err.Error())
+		return nil, err
 	}
 
+	p.Logger.Info("CreatePayment has successfully finished")
 	return resp, nil
 }
 
 func (p *PaymentService) GetPayment(ctx context.Context, req *pb.ID) (*pb.PaymentInfo, error) {
+	p.Logger.Info("GetPayment method is starting")
+
 	resp, err := p.Repo.GetPayment(ctx, req.Id)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to read payment")
+		err := errors.Wrap(err, "failed to read payment")
+		p.Logger.Error(err.Error())
+		return nil, err
 	}
 
+	p.Logger.Info("GetPayment has successfully finished")
 	return resp, nil
 }
 
 func (p *PaymentService) UpdatePayment(ctx context.Context, req *pb.PaymentInfo) (*pb.Void, error) {
+	p.Logger.Info("UpdatePayment method is starting")
+
 	err := p.Repo.UpdatePayment(ctx, req)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to update payment")
+		err := errors.Wrap(err, "failed to update payment")
+		p.Logger.Error(err.Error())
+		return nil, err
 	}
 
+	p.Logger.Info("UpdatePayment has successfully finished")
 	return &pb.Void{}, nil
 }
 
 func (p *PaymentService) SearchByReservationID(ctx context.Context, req *pb.ID) (*pb.PaymentInfo, error) {
+	p.Logger.Info("SearchByReservationID method is starting")
+
 	resp, err := p.Repo.SearchByReservationID(ctx, req.Id)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to find payment")
+		err := errors.Wrap(err, "failed to find payment")
+		p.Logger.Error(err.Error())
+		return nil, err
 	}
 
+	p.Logger.Info("SearchByReservationID has successfully finished")
 	return resp, nil
 }
